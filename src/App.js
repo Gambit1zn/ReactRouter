51ae9804e7a92af9d0e9ac89e5809da375509b3d
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-06-18 13:09:34 +0200

    added react router and a link, added defined routes and implemented react router for basic use

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+//alternate way of defining routes in react router (mostly seen in the older versions of react router)
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element= {<HomePage></HomePage>}></Route>
+//     <Route path='/products' element= {<ProductsPage></ProductsPage>}></Route>
+//   </Route>
+// );
+ 
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage></HomePage>},
+  {path: '/products', element: <ProductsPage/>}
+
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+  return <h3>Products page</h3>
+}
+
+export default ProductsPage;
