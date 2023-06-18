272a4b08ab2c0cab2865c7dbb525af9200185d00
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-06-18 16:35:54 +0200

    Added error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
 
 //alternate way of defining routes in react router (mostly seen in the older versions of react router)
 // const routeDefinitions = createRoutesFromElements(
@@ -15,6 +16,7 @@ import RootLayout from "./pages/Root";
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <Error/>,
     element: <RootLayout></RootLayout>,
     children: [
       { path: "/", element: <HomePage></HomePage> },
