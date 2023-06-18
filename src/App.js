f594b20e9dca8885a80971212c60f050b422bec6
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-06-18 14:04:02 +0200

    solved layout creation by using nested routes and outlets within react-router-dom, created layout page and added some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import HomePage from './pages/Home';
-import ProductsPage from './pages/Products';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 //alternate way of defining routes in react router (mostly seen in the older versions of react router)
 // const routeDefinitions = createRoutesFromElements(
@@ -9,11 +11,16 @@ import ProductsPage from './pages/Products';
 //     <Route path='/products' element= {<ProductsPage></ProductsPage>}></Route>
 //   </Route>
 // );
- 
-const router = createBrowserRouter([
-  {path: '/', element: <HomePage></HomePage>},
-  {path: '/products', element: <ProductsPage/>}
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+      { path: "/", element: <HomePage></HomePage> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
